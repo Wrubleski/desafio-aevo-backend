6bd6fc62b3b0f9c5aacb1db5c757b12008080ac0
Wrubleski <henrike.hip@live.com>
2021-02-06 14:10:45 -0300

    add comment as to why this wrapper exists

--- src/infrastructure/wrappers/RequestWrapper.js
+++ src/infrastructure/wrappers/RequestWrapper.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+// If i ever need to change the HTTP client, with this wrapper I`ll only need to change it in one place.
 class RequestWrapper {
   async getRequest(path, params) {
     const request = await axios({
